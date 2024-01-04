5ee90fb4fa458ffa2606c7aefce5d0401643c18a
Hafiz Muhammad Ali Sajjad <70126786+hmalisajjad@users.noreply.github.com>
2024-01-05 00:46:45 +0100

    complete

--- students/views.py
+++ students/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from types import NoneType
+from .models import *
 # Create your views here.
 
 def home(request):
-    students = None
+    students = Student.objects.all()
     return render(request,'home.html',{'students':students})
 
 def student(request, student_id):
-    student = None
+    try:
+        student = Student.objects.get(id=student_id)
+    except Student.DoesNotExist:
+        student = None
     return render(request,'student.html',{'student':student})
