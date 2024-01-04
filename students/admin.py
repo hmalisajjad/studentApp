1e172c028a6c39bfd810cccd162ea2bad65e5133
Hafiz Muhammad Ali Sajjad <70126786+hmalisajjad@users.noreply.github.com>
2024-01-04 23:21:29 +0100

    applying CRUD operations

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "last_name", "email_address"]
 
+@admin.register(Departments)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ["department_name"]
 # Register your models here.
